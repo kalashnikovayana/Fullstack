a4c7101e978026ff19c80ddf417f46e2ed7b491c
Yana Kalashnikova <yana.kalashnikova2704@gmail.com>
2026-09-12 20:06:18 +0300

    Update home-work-0

--- home-work-0/src/images.js
+++ home-work-0/src/images.js
@@ -8,17 +8,18 @@
 // Every source photo is 1672×941 (native size, used for width/height to
 // avoid layout shift; display size is controlled by CSS).
 export const PHOTO_SIZE = { width: 1672, height: 941 };
+export const HERO_SIZE = { width: 1965, height: 2944 };
 
 export const SERVICE_IMG = {
-  "hair-color": "/images/svc-hair-color.png",
-  "haircut": "/images/svc-haircut.png",
-  "care": "/images/svc-care.png",
-  "manicure": "/images/svc-manicure.png",
-  "pedicure": "/images/svc-pedicure.png",
-  "face": "/images/svc-face.png",
-  "brows": "/images/svc-haircut.png",
-  "makeup": "/images/svc-care.png",
-  "spa": "/images/svc-pedicure.png",
+  "hair-color": "/images/salon-blow-drying-hair.jpg",
+  "haircut": "/images/woman-getting-hair-cut-at-salon.jpg",
+  "care": "/images/woman-stylist-cuts-hair-at-salon.jpg",
+  "manicure": "/images/hands-with-nail-polish-line-up-fabric.jpg",
+  "pedicure": "/images/b6907119-6469-4e60-8c1a-cec5925ee309.png",
+  "face": "/images/woman-opening-cosmetic-jar.jpg",
+  "brows": "/images/woman-getting-makeup-applied.jpg",
+  "makeup": "/images/woman-getting-makeup-in-natural-light.jpg",
+  "spa": "/images/massage-therapy-upper-back.jpg",
 };
 
 export const SERVICE_ICON = {
@@ -40,11 +41,11 @@ export const MASTER_IMG = {
   sofia: "/images/m-sofia.png",
 };
 
-export const HERO_IMG = "/images/m-olha.png";
+export const HERO_IMG = "/images/a-model-in-summery-clothes.jpg";
 
 export const GALLERY = [
   { id: "g2", src: SERVICE_IMG["hair-color"], cls: "g-tall" },
-  { id: "g3", src: SERVICE_IMG["haircut"] },
+  { id: "g3", src: "/images/woman-stylist-cuts-hair-at-salon.jpg" },
   { id: "g4", src: SERVICE_IMG["manicure"] },
   { id: "g5", src: SERVICE_IMG["pedicure"], cls: "g-wide" },
   { id: "g6", src: SERVICE_IMG["face"] },
@@ -52,9 +53,9 @@ export const GALLERY = [
 ];
 
 export const BLOG_IMG = {
-  "b-1": SERVICE_IMG["hair-color"],
+  "b-1": "/images/woman-stylist-cuts-hair-at-salon.jpg",
   "b-2": SERVICE_IMG["manicure"],
-  "b-3": SERVICE_IMG["face"],
+  "b-3": "/images/getting-makeup-done.jpg",
 };
 
 const TINTS = ["rose", "gold", "teal"];
